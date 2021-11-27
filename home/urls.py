0c21261992496306011c680b783096a6971090f7
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-27 11:50:29 +0330

    created url for index and about view

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from .views import *
+
+app_name = 'home'
+
+
+urlpatterns = [
+    path('', index_view, name='index'),
+    path('about', about_view, name='about'),
+]
