e6af4ee0150e09038e2ffff1cbcaf48c8f4121a2
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-27 15:25:07 +0330

    wrote view for index_view (main page of blog)

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    pass
+    return render(request, 'home/index.html')
 
 def about_view(request):
     return render(request, 'home/about.html')
