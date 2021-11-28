018ba751a230e0bf07f2e4afe56c6bdffb487ccf
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-28 09:15:37 +0330

    wrote index_view and detail_view for index url and detail url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    return render(request, 'blog/blog.html')
+
+def detail_view(request):
+    return render(request, 'blog/detail.html')
