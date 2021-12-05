6acd46989bdeac7ec70a501910eff0073c671e2a
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-05 09:43:58 +0330

    customize admin model

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,10 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class AdminPost(admin.ModelAdmin):
-    pass
+    date_hierarchy = "published_date"
+    # exclude = ['title', 'content']
+    list_display = ['title', 'status', 'published_date']
+    list_filter = ['status']
+    ordering = ['-created_date']
+    search_fields = ['title', 'content']
 
