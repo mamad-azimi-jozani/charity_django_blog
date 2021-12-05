b9026a9fcae36b98a6c6ea9eb2f9188a8004d1f4
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-05 09:56:19 +0330

    more complete two view I mean in index show all the post but in detail show post by id

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def index_view(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, 'blog/blog.html', context)
 
-def detail_view(request):
-    return render(request, 'blog/detail.html')
+def detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {
+        "post" : post
+    }
+    return render(request, 'blog/detail.html', context)
