75b517494bd82f862c005ac0ede8d9c8c19e3f48
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-05 10:55:45 +0330

    made query with get_object_or_404 and correct one bug in detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 # Create your views here.
 def index_view(request):
@@ -7,7 +7,7 @@ def index_view(request):
     return render(request, 'blog/blog.html', context)
 
 def detail_view(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id, status=1)
     context = {
         "post" : post
     }
