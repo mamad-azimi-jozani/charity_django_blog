4c511bd41ba3ed169b1ed4058e31f94ed2bdffb0
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-06 09:12:24 +0330

    added author field to list_display to show each post by author

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class AdminPost(admin.ModelAdmin):
     date_hierarchy = "published_date"
     # exclude = ['title', 'content']
-    list_display = ['title', 'status', 'published_date']
+    list_display = ['title', 'status', 'author', 'published_date']
     list_filter = ['status']
     ordering = ['-created_date']
     search_fields = ['title', 'content']
