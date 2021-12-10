ef9b91223922d9e4efb0f1bdd77702bb88241155
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-10 12:10:05 +0330

    created simple_tag for showing last five post

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,9 @@
 from django import template
-
+from ..models import Post
+from django.shortcuts import render
 register = template.Library()
 
+@register.simple_tag(name='recent_posts')
+def sample():
+    posts = Post.objects.filter(status=1)[:5]
+    return posts
