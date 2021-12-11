57b11af9d49f636a26a336a8967177409281c9b8
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-11 15:44:03 +0330

    connect two url to one view because they were the same

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', index_view, name='index_blog'),
+    path('category/<str:cat_name>', index_view, name='category'),
     path('detail/<int:post_id>', detail_view, name='detail'),
-    path('category/<str:cat_name>', category_view, name='category'),
+    
 ]
