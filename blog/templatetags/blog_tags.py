cb3de6da2a3ec2a3f997e4e4fbe24c4837cec61e
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-11 11:19:37 +0330

    wrote simple tag for showing each post how many category have

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Post
+from ..models import Post, Category
 from django.shortcuts import render
 register = template.Library()
 
@@ -7,3 +7,12 @@ register = template.Library()
 def sample():
     posts = Post.objects.filter(status=1)[:5]
     return posts
+
+@register.simple_tag(name='post_category')
+def sample():
+    posts = Post.objects.filter(status=1)
+    cat = Category.objects.all()
+    cat_dict = {}
+    for i in cat:
+        cat_dict[i] = posts.filter(category=i).count()
+    return cat_dict
