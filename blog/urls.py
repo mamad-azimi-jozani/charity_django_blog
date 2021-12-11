f2af0108b5614d96dc55c8f214dd10e9aa1916da
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-11 12:29:58 +0330

    added new url for showing post by category

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', index_view, name='index_blog'),
     path('detail/<int:post_id>', detail_view, name='detail'),
+    path('category/<str:cat_name>', category_view, name='category'),
 ]
