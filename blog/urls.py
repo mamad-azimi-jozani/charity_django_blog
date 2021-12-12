390023c491ba896fb1b3700e6c606982f13e5b6f
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-12 07:59:45 +0330

    wrote new url for filtering post by username

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', index_view, name='index_blog'),
     path('category/<str:cat_name>', index_view, name='category'),
+    path('author/<str:author_name>', author_view, name='author'),
     path('detail/<int:post_id>', detail_view, name='detail'),
-    
+
 ]
