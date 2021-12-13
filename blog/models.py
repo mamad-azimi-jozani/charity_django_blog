16a6c2413bbf474e910a0699756a77de44f72f4d
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-13 09:26:03 +0330

    added Meta class for ordering by published_date

--- blog/models.py
+++ blog/models.py
@@ -21,5 +21,8 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['published_date']
+
     def __str__(self):
         return self.title
