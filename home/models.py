f3cbd5c3bb77e9f617fe54054dc359f9fa75e591
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-15 11:52:36 +0330

    wrote magic func for showing obj by name

--- home/models.py
+++ home/models.py
@@ -9,4 +9,6 @@ class Contact(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
