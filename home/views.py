4931d913eef9d5aa9ad0c5c45e09cc963e2065f5
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-15 21:38:14 +0330

    completed contact view and added form validation to it

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from .models import Contact
+from django.http import HttpResponse
 
 # Create your views here.
 def index_view(request):
@@ -8,4 +11,12 @@ def about_view(request):
     return render(request, 'home/about.html')
 
 def contact_view(request):
-    return render(request, 'home/contact.html')
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'home/contact.html', context)
