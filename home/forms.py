dff5f0ba8cbc71c44b9e9c5c70f5bff5ee2cde6a
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-17 22:27:32 +0330

    created customize error_message

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    name = forms.CharField(error_messages={
+        'required': 'Please enter your name'
+    })
+    email = forms.EmailField(error_messages={
+        'required': 'Please enter your email'
+    })
+    subject = forms.CharField(error_messages={
+        'required': 'Please enter subject'
+    })
+    message = forms.CharField(widget=forms.Textarea, error_messages={
+        'required': 'please enter message'
+    })
     class Meta:
         model = Contact
         fields = "__all__"
