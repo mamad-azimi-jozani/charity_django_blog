61412524f4e2c7ef82b4d7a45ec1aea8ddada024
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-22 17:10:56 +0330

    added summernote to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from .models import Post, Category
 # Register your models here.
 @admin.register(Post)
-class AdminPost(admin.ModelAdmin):
+class AdminPost(SummernoteModelAdmin):
     date_hierarchy = "published_date"
     # exclude = ['title', 'content']
     list_display = ['title', 'status', 'author', 'published_date']
     list_filter = ['status']
     ordering = ['-created_date']
     search_fields = ['title', 'content']
+    summernote_fields = ('content',)
 
 @admin.register(Category)
 class AdminCategory(admin.ModelAdmin):
