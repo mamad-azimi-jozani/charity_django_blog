9ee70a3d6af1fc5ad7bb2b63909987076f4bbbb8
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-12-22 10:42:49 +0330

    added a new url for filtering tag

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('category/<str:cat_name>', index_view, name='category'),
     path('author/<str:author_name>', index_view, name='author'),
     path('detail/<int:post_id>', detail_view, name='detail'),
+    path('tag/<str:tag_name>', index_view, name='tag'),
     path('search/', blog_search, name='search'),
 
 ]
